47cb486a0ea3862897bfcbe487837bd1c94c1282
syunkitada <syun.kitada@gmail.com>
2019-12-15 12:47:41 +0900

    update

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -12,6 +12,11 @@ sudo apt-get install -y neovim
 sudo apt-get install -y python3-dev python3-pip
 sudo pip install neovim
 
+# exuberant-ctagsはメンテが止まっており、universal-ctagsに移行している途中(2019/12/15)
+# sudo snap install universal-ctags
+sudo apt-get install exuberant-ctags
+curl https://raw.githubusercontent.com/jb55/typescript-ctags/master/.ctags > ~/.ctags
+
 # install fzf
 if [ ! -e ~/.fzf ]; then
     git clone https://github.com/junegunn/fzf.git ~/.fzf
